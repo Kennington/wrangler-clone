aa58c5aca3178d338fc2efe50bfab901d6235807
Kenny Lu <Kennington@Kennys-MacBook.local>
2015-07-22 19:11:19 +0800

    finished smallPhones landing page

---
+++ client/templates/header.js
@@ -0,0 +1,19 @@
+Template.header.events ({
+	'click #facebook': function (e) {
+		e.PreventDefault();
+
+		// Router.go ('GoToFacebook');
+	},
+
+	'click #instagram': function (e) {
+		e.PreventDefault();
+
+		// Router.go ('GoToFacebook');
+	},
+
+	'click #twitter': function (e) {
+		e.PreventDefault();
+
+		// Router.go ('GoToFacebook');
+	}
+});
